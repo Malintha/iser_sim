238072f1772e49da8870a67d5eda8683b509eee0
Malintha Fernando <malinthak2@gmail.com>
2020-07-05 00:52:41 -0400

    add obstacle visualization

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,46 @@
 #include "ros/ros.h"
 #include <iostream>
 #include <ros/console.h>
-#include "Trajectory.h"
-#include "Visualize.h"
-
+#include "swarmsim/Trajectory.h"
+#include "swarmsim/Visualize.h"
+#include "swarmsim/utils.h"
+#include <geometry_msgs/Point.h>
 
 using namespace std;
 
+typedef std::vector<geometry_msgs::Point> Grid;
+
 int main(int argc, char **argv) {
     float frequency = 100;
-    ros::init(argc, argv, "prim_vis");
+    ros::init(argc, argv, "prm_vis");
     static ros::NodeHandle n("~");
     string obstacleConfigFileName;
-    string trajDir;
+    string dataDir;
+    int nDrones;
 
-    n.getParam("/prim_vis/obstacleFileName", obstacleConfigFileName);
-    n.getParam("/prim_vis/traj_dir", trajDir);
-    
-
+    n.getParam("/prm_vis/obstacleFileName", obstacleConfigFileName);
+    n.getParam("/prm_vis/data_dir", dataDir);
+    n.getParam("/prm_vis/nDrones", nDrones);
 
     ROS_DEBUG_STREAM("obstacleConfigFileName: " << obstacleConfigFileName);
-    ROS_DEBUG_STREAM("obstacleConfigFileName: " << obstacleConfigFileName);
+    ROS_DEBUG_STREAM("dataDir: " <<dataDir);
+    vector<Trajectory> trajectories = simutils::loadTrajectoriesFromFile(nDrones, n, dataDir);
 
+    stringstream ss;
+    ss << dataDir << obstacleConfigFileName;
+    string obstacleConfigPath = ss.str();
+    Visualize vis(n, "map", nDrones, obstacleConfigPath);
+    vis.addToPaths(trajectories);
+
+
+    while(ros::ok()) {
+        vis.draw();
+    }
 
     return 0;
 }
+
+Grid getGridSampling() {}
+
+Grid getSampling(Grid pathNodes) {}
+
