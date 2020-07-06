f1e3043277d1e14ac7e3e135905dc8f5d85a2e7f
Malintha Fernando <malinthak2@gmail.com>
2020-07-06 17:11:33 -0400

    adding schedule read

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "swarmsim/Visualize.h"
 #include "swarmsim/utils.h"
 #include <geometry_msgs/Point.h>
+#include <fstream>
 
 using namespace std;
 
@@ -19,22 +20,59 @@ struct visProp {
 Grid getGridSampling() {
     double len = 40;
     double wid = 40;
-    double hei = 1;
-    double incre = 1;
+    double hei = 5;
+    double incre = 5;
+    double h_ = 4;
 
     Grid grid;
     for(double l=0;l<=len;l+=incre) {
         for(double w=0;w<=wid;w+=incre) {
-            for(double h=0;h<=hei;h+=incre) {
+            for(double h=h_;h<=hei;h+=incre) {
                 geometry_msgs::Point p;
                 p.x = l;
                 p.y = w;
                 p.z = h;
-                grid.push_back(p);
+                grid.emplace_back(p);
             }
         }
     }
     return grid;
+}
+
+Grid getStartGoalPositions(std::string startsConfigPath) {
+    std::ifstream infile(startsConfigPath);
+    int x, y, z;
+    Grid positions;
+    while(infile >> x >> y >> z) {
+        geometry_msgs::Point p;
+        p.x = x;
+        p.y = y;
+        p.z = z;
+        positions.push_back(p);
+    }
+    ROS_ERROR_STREAM("Start positions: "<<positions.size());
+return positions;
+}
+
+std::vector<Grid> getAgentSchedules(int nAgents, std::string dataDir) {
+    stringstream ss;
+    std::vector<Grid> schedule;
+    for(int i=1; i<=nAgents; i++) {
+        ss << dataDir << "agent_" << std::to_string(i)<<".txt";
+        std::ifstream infile(ss.str());
+        int x, y, z;
+        Grid path;
+        while(infile >> x >> y >> z) {
+            geometry_msgs::Point p;
+            p.x = x;
+            p.y = y;
+            p.z = z;
+            path.push_back(p);
+            std::cout<<"point: "<<x<<" "<<y<<" "<<z<<std::endl;
+        }
+    schedule.push_back(path);
+    }
+    return schedule;
 }
 
 int main(int argc, char **argv) {
@@ -43,14 +81,23 @@ int main(int argc, char **argv) {
     static ros::NodeHandle n("~");
     string obstacleConfigFileName;
     string dataDir;
-    int nDrones;
-
+    int nAgents, nDrones;
+    string startsConfigName;
+    nAgents = 2;
     n.getParam("/prm_vis/obstacleFileName", obstacleConfigFileName);
     n.getParam("/prm_vis/data_dir", dataDir);
     n.getParam("/prm_vis/nDrones", nDrones);
+    n.getParam("/prm_vis/startsConfigName", startsConfigName);
 
     ROS_DEBUG_STREAM("obstacleConfigFileName: " << obstacleConfigFileName);
     ROS_DEBUG_STREAM("dataDir: " <<dataDir);
+    stringstream ss_;
+    ss_ << dataDir << startsConfigName;
+    string startsFilePath = ss_.str();
+    Grid positions = getStartGoalPositions(startsFilePath);
+    std::vector<Grid> schedule = getAgentSchedules(nAgents, dataDir);
+    ROS_ERROR_STREAM("schedule length: "<<schedule.size());
+
     vector<Trajectory> trajectories = simutils::loadTrajectoriesFromFile(nDrones, n, dataDir);
 
     stringstream ss;
@@ -62,6 +109,7 @@ int main(int argc, char **argv) {
     // get grid sampling
     Grid grid = getGridSampling();
     vis.addToGrid(grid);
+    vis.addStartGoal(positions);
 
     while(ros::ok()) {
         vis.draw();
@@ -71,5 +119,7 @@ int main(int argc, char **argv) {
 }
 
 
-Grid getSampling(Grid pathNodes) {}
+Grid getSampling(Grid pathNodes) {
+
+}
 
